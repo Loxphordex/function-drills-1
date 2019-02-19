1850d5fd68cd26b1c1b954f0f7613f3783fee639
mak20th <mak20th@gmail.com>
2019-02-19 15:36:28 -0600

    added a return statement

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 function createGreeting() {
-  console.log('Hi, my name is Aamir, and I\'m 18 years old');
+  return('Hi, my name is Aamir, and I\'m 18 years old');
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
