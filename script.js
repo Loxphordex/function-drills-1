dd7218c671226c43ebefa1ceeb96091189c688bf
mak20th <mak20th@gmail.com>
2019-02-19 15:49:47 -0600

    updated js file

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 'use strict';
-function createGreeting(name, age) {
-    const yearOfBirth = 2019 - age;
-    return(`Hi, my name is ${name}, and I'm ${age}years old. I was born in ${yearOfBirth}`);
+function createGreeting(name, age) { 
+  const yob = getYearOfBirth(age);   
+  return(`Hi, my name is ${name}, and I'm ${age}years old. I was born in ${yob}`);
+}
+
+function getYearOfBirth(age){
+  const yearOfBirth = 2019 - age;
+  return yearOfBirth;
 }
 
 const greeting1 = createGreeting();
